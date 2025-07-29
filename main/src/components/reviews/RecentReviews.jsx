aaa5de6ba22d7cef59a40598caa67b0140e13e8c
johnmartinroque <johnmartinroque@gmail.com>
2025-07-29 20:01:26 +0800

    firebase stuff

--- main/src/components/reviews/RecentReviews.jsx
+++ main/src/components/reviews/RecentReviews.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { db } from "../../firebase";
+import { getDoc, getDocs } from "firebase/firestore";
 
 function RecentReviews() {
   return <div>RecentReviews</div>;
