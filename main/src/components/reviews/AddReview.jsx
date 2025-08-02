536284b97e18ba440a88bc3fabf465a4cd16d0e7
johnmartinroque <johnmartinroque@gmail.com>
2025-08-02 20:00:24 +0800

    watched, liked, add

--- main/src/components/reviews/AddReview.jsx
+++ main/src/components/reviews/AddReview.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { Card } from "react-bootstrap";
 import { Rating } from "react-simple-star-rating";
-import "../../css/AddReview.css";
 
 function AddReview() {
   const [rating, setRating] = useState(0); // default value
@@ -17,6 +16,11 @@ function AddReview() {
       <Card style={{ height: "40rem", padding: "1rem" }}>
         <Card.Title style={{ color: "black" }}>Add Your Review</Card.Title>
         <Card.Body>
+          <Card.Text>
+            <i class="fa-solid fa-eye"></i>
+            <i class="fa-solid fa-heart" style={{ color: "#ff8000" }}></i>
+            <i class="fa-solid fa-plus"></i>
+          </Card.Text>
           <Card.Text>
             <Rating
               onClick={handleRating}
