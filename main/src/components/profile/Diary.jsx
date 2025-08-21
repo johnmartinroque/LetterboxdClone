0f6501c07ac976c6b3206f7377685a0d0c5c15c3
johnmartinroque <johnmartinroque@gmail.com>
2025-08-21 11:24:56 +0800

    DIARY CSS

--- main/src/components/profile/Diary.jsx
+++ main/src/components/profile/Diary.jsx
@@ -1,39 +1,40 @@
 import React from "react";
 import Table from "react-bootstrap/Table";
-
+import "../../css/Diary.css";
 function Diary() {
   return (
     <div>
       <Table striped bordered hover>
         <thead>
           <tr>
-            <th>MONTH</th>
-            <th>DAY</th>
-            <th>FILM</th>
-            <th>RELEASED</th>
-            <th>RATING</th>
-            <th>LIKE</th>
-            <th>REWATCH</th>
+            <th className="month-column">MONTH</th>
+            <th className="day-column">DAY</th>
+            <th className="film-column">FILM</th>
+            <th className="released-column">RELEASED</th>
+            <th className="rating-column">RATING</th>
+            <th className="like-column">LIKE</th>
+            <th className="rewatch-column">REWATCH</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td colSpan={2}>Larry the Bird</td>
-            <td>@twitter</td>
-          </tr>
+          {[1, 2, 3].map((_, index) => (
+            <tr key={index}>
+              <td className="month-column">JULY 2025</td>
+              <td className="day-column">23</td>
+              <td className="film-column">Fantastic Four</td>
+              <td className="released-column">2025</td>
+              <td className="rating-column">
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+              </td>
+              <td className="like-column">
+                <i className="fa-solid fa-heart"></i>
+              </td>
+              <td className="rewatch-column">
+                <i className="fa-solid fa-repeat"></i>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </Table>
     </div>
