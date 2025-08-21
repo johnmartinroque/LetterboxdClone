4e5d16c090039062cb1dee01f9c0cfb2f1155985
johnmartinroque <johnmartinroque@gmail.com>
2025-08-21 11:12:05 +0800

    table react bootstrap

--- main/src/components/profile/Diary.jsx
+++ main/src/components/profile/Diary.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import Table from "react-bootstrap/Table";
 
 function Diary() {
-  return <div>Diary</div>;
+  return (
+    <div>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>MONTH</th>
+            <th>DAY</th>
+            <th>FILM</th>
+            <th>RELEASED</th>
+            <th>RATING</th>
+            <th>LIKE</th>
+            <th>REWATCH</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td colSpan={2}>Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </Table>
+    </div>
+  );
 }
 
 export default Diary;
